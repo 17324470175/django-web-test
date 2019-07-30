922adf3a43b66de3cf898c7c8fbeadc2e40eddce
系统全局用户名 <@qq.com>
2019-07-30 22:10:26 +0800

    '增加项目Model'

---
+++ comment/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from blog.models import Post
+# Create your models here.
+
+
+class Comment(models.Model):
+    STATUS_NORMAL = 1
+    STATUS_DELETE = 0
+    STATUS_ITEMS = (
+        (STATUS_NORMAL, '正常'),
+        (STATUS_DELETE, '删除'),
+    )
+    # 在django2.0后，定义外键和一对一关系的时候需要加on_delete选项，此参数为了避免两个表里的数据不一致问题，不然会报错
+    target = models.ForeignKey(Post, verbose_name="评论目标", on_delete=models.CASCADE)
+    content = models.CharField(max_length=2000, verbose_name="内容")
+    nickname = models.CharField(max_length=50, verbose_name="昵称")
+    website = models.URLField(verbose_name="网站")
+    email = models.EmailField(verbose_name="邮箱")
+    status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name="状态")
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+
+    class Meta:
+        verbose_name = verbose_name_plural = "评论"
